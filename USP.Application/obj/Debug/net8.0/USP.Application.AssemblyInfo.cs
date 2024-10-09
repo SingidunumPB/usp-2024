0c1579e2222d5f69bdf6524cddfa2c38287b7394
pbisevac <pbisevac@singimail.rs>
2024-10-09 12:36:11 +0200

    .
    .

--- USP.Application/obj/Debug/net8.0/USP.Application.AssemblyInfo.cs
+++ USP.Application/obj/Debug/net8.0/USP.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USP.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c4c2605d5560dc46f46d5e834ab1f70e378a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("USP.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USP.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- USP.Infrastructure/obj/Debug/net8.0/USP.Infrastructure.AssemblyInfo.cs
+++ USP.Infrastructure/obj/Debug/net8.0/USP.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USP.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c4c2605d5560dc46f46d5e834ab1f70e378a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("USP.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USP.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
